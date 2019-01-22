316b0f1631ce6328afe73c5d5e8a6ab6773d7590
Riley-Brown <riley@riley.gg>
2019-01-21 22:05:58 -0500

    Updated carousel display based on hover state

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const landingH2 = document.querySelector('.fullscreen-landing h2');
 const images = ['../img/img-1.jpeg', '../img/img-2.jpeg', '../img/img-3.jpeg']
 
 // image tag inside image carousel div
-let imageArray = document.querySelector('.image-array')
+let imgDiv = document.querySelector('.image-array')
 
 // event for mouse over on main landing 
 landingH2.addEventListener('mouseover', carouselOnHover)
@@ -25,6 +25,9 @@ let isPaused = false;
 function carouselOnHover() {
   isPaused = false;
 
+  // sets img tag in carousel div to block
+  imgDiv.style.display = 'block';
+
   // sets first initial image on hover
   document.querySelector('.image-array').src = images[index]
 
@@ -38,7 +41,7 @@ function carouselOnHover() {
       index = 0
     }
 
-    imageArray.src = images[index]
+    imgDiv.src = images[index]
 
   }, 1000)
   /* ===== END OF INTERVAL ===== */
@@ -48,6 +51,7 @@ function carouselOnHover() {
     isPaused = true;
     index = 0;
     clearInterval(int)
+    imgDiv.style.display = 'none';
   })
 
 }
