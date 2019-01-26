502d50ad0ca268caa6b3d3922559f0856937a1d2
Riley-Brown <riley@riley.gg>
2019-01-25 21:23:12 -0500

    Made hover function dynamic allowing different DOM elements, length before next element, and a image or video to be passed in

--- js/index.js
+++ js/index.js
@@ -1,59 +1,75 @@
-// h2 on main landing
-const landingH2 = document.querySelector('.fullscreen-landing h2');
+// // h2 on main landing
+// const landingH2 = document.querySelector('.fullscreen-landing h2');
 
-// image carousel array
+// image array for Work hover
 const images = ['img/img-1.jpeg', 'img/img-2.jpeg', 'img/img-3.jpeg'];
 
-// image tag inside image carousel div
-let imgDiv = document.querySelector('.image-array')
+// video array for About hover
+const textVidArr = ['video/placeholder-vid.mp4', 'video/placeholder-vid-2.mp4'];
 
-// event for mouse over on main landing 
-landingH2.addEventListener('mouseover', carouselOnHover)
+// div that holds content on hover
+let imgDiv = document.querySelector('.image-carousel');
 
+// all span tags on landing for hover event
+let hoverElements = document.querySelectorAll('.fullscreen-landing div span');
 
-let index = 0;
+// adds event for 2nd span
+hoverElements[1].addEventListener('mouseover', function () {
+  onHover(hoverElements[1], images, 'img', 700);
+});
 
+// event for 1st span
+hoverElements[0].addEventListener('mouseover', function () {
+  onHover(hoverElements[0], textVidArr, 'video', 3000);
+});
 
-function carouselOnHover() {
+function onHover(element, imgArr, type, length) {
+
+  let index = 0;
 
   // Fades in image carousel with jQuery
-  $('.image-array').fadeIn();
+  $('.image-carousel').fadeIn(400);
 
-  // sets first initial image on hover
-  document.querySelector('.image-array').src = images[index]
+  // checks element type passed into function
+  if (type == 'img') {
+    $('.image-carousel')
+      .html(`<img src=${imgArr[index]} alt="portfolio" class="image-array">`);
+  } else if (type == 'video') {
+    $('.image-carousel')
+      .html(`<video src=${imgArr[index]} class="image-array" autoplay muted loop></video>`);
+  }
 
-  // interval to increase image index 
+  // interval to increase array index 
   let int = setInterval(function () {
 
     // increase value every interval
     index++
 
-    // resets index to 0 if longer than image array
-    if (index >= images.length) {
+    // resets index to 0 if longer than array length
+    if (index >= imgArr.length) {
       index = 0
     }
 
-    // sets new image every interval
-    imgDiv.src = images[index]
+    // sets new src every interval
+    document.querySelector('.image-array').src = imgArr[index]
 
-  }, 700)
-  // Interval end
+  }, length) // dynamic length based on value passed in
 
-  // mouse leave event to clear interval and hide image carousel
-  landingH2.addEventListener('mouseleave', function () {
+  // mouse leave event to clear interval and hide carousel
+  element.addEventListener('mouseleave', function () {
 
     index = 0;
     clearInterval(int)
 
-    // Fade out image carousel with jQuery 
-    $('.image-array').fadeOut();
+    // Fade out carousel with jQuery 
+    $('.image-carousel').fadeOut(10);
   })
 
 }
 
 
-// event to open menu on landing h2 click
-landingH2.addEventListener('click', openMenu);
+// event to open menu on Work span click
+hoverElements[1].addEventListener('click', openMenu);
 
 // event to close menu on emoji click
 $('.gesture-emoji').click(function () {
@@ -71,5 +87,4 @@ function openMenu() {
 
   // adds class to blur landing content
   $('.fullscreen-landing').addClass('fullscreen-landing-blur');
-
 }
